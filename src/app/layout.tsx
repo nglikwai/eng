d7eb030e7fd0948522c791c3a2053cf8ca5e7004
Alex Lam <alexlamazinga@gmail.com>
2025-01-25 22:00:16 +0800

    chore: setup react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import type { Metadata } from 'next';
 
+import ReactQueryClientProvider from '@/providers/ReactQueryClientProvider';
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
       </body>
     </html>
   );
